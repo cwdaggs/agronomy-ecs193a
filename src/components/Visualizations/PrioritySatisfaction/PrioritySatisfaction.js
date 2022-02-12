7bf307d861983d1d7f1fdc17669951adb1141c88
Akosnikowski <78250440+Akosnikowski@users.noreply.github.com>
2022-02-12 13:13:24 -0800

    minor changes

--- src/components/Visualizations/PrioritySatisfaction/PrioritySatisfaction.js
+++ src/components/Visualizations/PrioritySatisfaction/PrioritySatisfaction.js
@@ -1,7 +1,8 @@
 import {VictoryAxis, VictoryLabel, VictoryTooltip, VictoryLine, VictoryChart, VictoryScatter, VictoryTheme} from 'victory';
 import { averageSatisfaction, filterByCrop, trendLineSatisfactions } from '../UseData';
 import * as d3 from 'd3'
-export function PrioritySatisfaction({dataset, filter}) {
+
+export const PrioritySatisfaction = ({dataset, filter}) => {
 
     if (!dataset) {
         return <pre>Loading...</pre>;
@@ -43,7 +44,7 @@ export function PrioritySatisfaction({dataset, filter}) {
                         fill: "#c43a31",
                         stroke: "#756f6a",
                         fillOpacity: 0.7,
-                        strokeWidth: 0.5,
+                        strokeWidth: 0.5
                     },
                     axis: {stroke: "#756f6a"},
                     ticks: {stroke: "grey", size: 5},
@@ -58,10 +59,14 @@ export function PrioritySatisfaction({dataset, filter}) {
                         fontSize:fontSize
                         }}
                         flyoutHeight={10}
-                        flyoutWidth={30}    
+                        flyoutWidth={30}
+                        dy={-3}    
                     />
                 }
                 
+                onMouseEnter={(props) => props.style.data.stroke = "black"}
+                onMouseLeave={(props) => props.style.data.stroke = "#756f6a"}
+
                 />
                 <VictoryAxis dependentAxis
                     label="Satisfaction"
@@ -83,7 +88,7 @@ export function PrioritySatisfaction({dataset, filter}) {
                         }}
                 />
             <VictoryLine
-                label="Trend Line"
+                
                 style={{ 
                     data: { 
                         stroke: "#756f6a", 
@@ -91,6 +96,36 @@ export function PrioritySatisfaction({dataset, filter}) {
                         strokeLinecap: "round" 
                     } 
                 }}
+                events={[{
+                    target: "parent",
+                    eventHandlers: {
+                      onMouseOver: () => {
+                        return [
+                          {
+                            target: "data",
+                            eventKey: "all",
+                            mutation: ({ style }) => {
+                              return style.stroke === "red"
+                                ? null
+                                : { style: { stroke: "black", strokeWidth: 1 } };
+                            }
+                          },
+                        ];
+                      },
+                      onMouseOut: () => {
+                        return [{
+                          target: "data",
+                          eventKey: "all",
+                          mutation: ({style}) => {
+                            return style.stroke === "red"
+                            ? null
+                            : { style: { stroke: "#756f6a", strokeWidth: 0.3 } };;
+                          }
+                        }];
+                      }
+                    }
+                  }]
+                }
                 x={(d) => d.x}
                 y={(d) => d.y}
                 data={trendData}
