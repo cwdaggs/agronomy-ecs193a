6901e3b4875c01bc3861a7f7eb9007411e9ca2f0
Christopher Pires <59940851+cmpires@users.noreply.github.com>
2022-02-17 17:26:33 -0800

    Update AcresManaged.js

--- src/components/Visualizations/AcresManagedVis/AcresManaged.js
+++ src/components/Visualizations/AcresManagedVis/AcresManaged.js
@@ -18,7 +18,7 @@ export function AcresManagedBarChart(props) {
           <VictoryChart height={300} width={600}
             domainPadding={10}
             padding={{left: 100, bottom: 30, top: 30, right: 100}}
-            animate={{duration: 500}}
+            animate={{duration: 700}}
           >
             <VictoryLabel text="Acres vs Number of Responses" x={225} y={20} textAnchor="right"/>
             <VictoryBar horizontal
