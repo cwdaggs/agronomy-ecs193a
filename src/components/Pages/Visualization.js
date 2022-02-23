93d5e4a375943fff1b90ac891a76c0970e24ebcd
Akosnikowski <78250440+Akosnikowski@users.noreply.github.com>
2022-02-23 11:24:48 -0800

    clean up

--- src/components/Pages/Visualization.js
+++ src/components/Pages/Visualization.js
@@ -19,15 +19,18 @@ export const Visualizations = () => {
     return(
         <div id="outer-container">
             
-                {types.map(type => (
-                    <Tab 
-                        key={type}
-                        icon={icons[type]} 
-                        onClick={() => {setActive(type)}} 
-                        active={active === type}
-                        >{type}
-                    </Tab>
-                 )) }
+            {types.map(type => (
+                <Tab 
+                    key={type}
+                    icon={icons[type]} 
+                    onClick={() => {setActive(type)}} 
+                    active={active === type}
+                    >{type}
+                </Tab>
+            ))}
+        
+            {<VisMenu dataset={useData('./data/Filtered_Crop_Data.csv')} vis={active}/>}
+
                 {/*
                 <MenuItem icon={<FaGem/>} onClick={() => {setActive(type);}} active={active === type} >Topic 1</MenuItem>
                 <MenuItem icon={<GiCorn/>}>Topic 2</MenuItem>
@@ -38,45 +41,7 @@ export const Visualizations = () => {
             {/* <SubMenu title="Components" icon={<FaEdgeLegacy />}>
             </SubMenu> */}
         
-        {<VisMenu dataset={useData('./data/Filtered_Crop_Data.csv')} vis={active}/>}
+        </div>
         
-        {/*{<VisMenu dataset={useData('./data/Full_Dataset.csv')} vis={active}/>}
-        <h2>Of these acres, in a given year estimate approximately what percentage are in the following categories?</h2>
-
-        {<VisMenu dataset={useData('./data/Full_Dataset.csv')} vis={"CropPercentages"}/>}
-             
-        {/*Question written in AcresManagedBarChart}
-        {<VisMenu dataset={useData('./data/Full_Dataset.csv')} vis={"AcresManaged"}/>}
-
-        <h2>Rate your current level of importance/satisfaction with UCCE's delivery of information on these topics, (1-3)</h2>
-        {<VisMenu dataset={useData('./data/Full_Dataset.csv')} vis={"PrioritySatisfaction"}/>}
-
-        <h2>How often do the following priorities affect your recommendations for field crop production?</h2>
-        {<VisMenu dataset={useData('./data/Full_Dataset.csv')} vis={"Affect"}/>}
-        */}
-      </div>
-        //     <div id="sidebar">
-        //         <ProSidebar id="sidebar-container">
-        //             <Menu iconShape="square" id="menu">
-        //                 <MenuItem icon={<FaGem/>} >Topic 1</MenuItem>
-        //                 <MenuItem icon={<GiCorn/>}>Topic 2</MenuItem>
-        //                 <MenuItem icon={<AiFillAndroid/>}>Topic 3</MenuItem>
-        //                 <MenuItem icon={<SiAdblock/>}>Topic 4</MenuItem>
-        //                 <MenuItem icon={<GrHorton/>}>Topic 5</MenuItem>
-        //                 {/*<SubMenu title="Components" icon={<FaEdgeLegacy />}></SubMenu>*/}
-        //             </Menu>
-        //         </ProSidebar>
-        //     </div>
-        //     {<VisMenu dataset={useData('./data/Grower_Crop_Data.csv')} vis={"Concerns"}/>}
-        //     {<CropPercentages dataset={useData('./data/Grower_Crop_Data.csv')}/>}
-        //     {<VisMenu dataset={useData('./data/Filtered_Crop_Data.csv')} vis={"AcresManaged"}/>}
-        //     {<VisMenu dataset={useData('./data/Grower_Crop_Data.csv')} vis={"PrioritySatisfaction"}/>}
-        //     {<VisMenu dataset={useData('./data/Filtered_Crop_data.csv')} vis={"Affect"}/>}
-        //     {<VisMenu dataset={useData('./data/Filtered_Crop_data.csv')} vis={"InfoSources"}/>}
-        //     {<VisMenu dataset={useData('./data/Filtered_Crop_data.csv')} vis={"InternetSources"}/>}
-        //     {<VisMenu dataset={useData('./data/Grower_Crop_data.csv')} vis={"PrimaryGrowingReasons"}/>}
-        //     {<VisMenu dataset={useData('./data/Grower_Crop_data.csv')} vis={"PriorityConcerns"}/>}
-
-        // </div>
     );
 }
