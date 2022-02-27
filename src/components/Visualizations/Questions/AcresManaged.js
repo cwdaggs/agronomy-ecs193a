0b4133328345a451ab2d3d24b20f80e42c78c7d8
cmpires <59940851+cmpires@users.noreply.github.com>
2022-02-27 12:17:58 -0800

    fixed my bar

--- src/components/Visualizations/Questions/AcresManaged.js
+++ src/components/Visualizations/Questions/AcresManaged.js
@@ -1,16 +1,55 @@
 import {VictoryLabel, VictoryAxis, VictoryChart, VictoryBar, VictoryTooltip} from 'victory';
-import {filterByCrop, calculateAcresManagedOrConsulted, calculateAcres} from '../UseData.js';
+import {filterByCrop} from '../UseData.js';
 import "typeface-abeezee";
+
+function calculateAcres(data){
+  var names = ["< 500 Acres", "< 1000 Acres", "< 1500 Acres", "< 2000 Acres", "< 2500 Acres", "2500+ Acres"]
+  var colors = ["#c9d2b7", "#b1b8a2", "#79917c", "#647766", "#343f36", "#212121"]
+  var columns = ["Acres_Managed", "Acres_Consulted"]
+  var modified_data=[]
+  var bin_count = [0,0,0,0,0,0]
+
+  for(var i = 0; i < data.length; i++){
+    for(var j = 0; j < columns.length; j++){
+      var num = parseInt(data[i][columns[j]], 10)
+      // Remove NAs and outliers
+      if(Number.isInteger(num)){
+          if(num > 10000){
+            continue;
+          }
+          if(num < 500){
+              bin_count[0]++
+          }
+          else if (num < 1000){
+            bin_count[1]++
+          }
+          else if (num < 1500){
+            bin_count[2]++
+          }
+          else if (num < 2000){
+            bin_count[3]++
+          }
+          else if (num < 2500){
+            bin_count[4]++
+          }
+          else{
+            bin_count[5]++
+          }   
+        }
+      }
+    }
+  for(var k=0; k<bin_count.length; k++){
+    modified_data.push({x: names[k], y: bin_count[k], fill: colors[k]});
+  }
+  return modified_data
+}
 
 export function AcresManagedBarChart(props) {
     if (!props.dataset) {
         return <pre>Loading...</pre>;
     }
-    //console.log(props.dataset)
     var data = filterByCrop(props.dataset, props.filter);
     var acre_data = calculateAcres(data);
-
-    const fontSize = 5;
 
     return (
         <div>
