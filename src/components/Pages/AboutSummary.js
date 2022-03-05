d5e32968b36cbc472bd062e5765a6df8f69ac826
cwdaggs <wmanu2013@gmail.com>
2022-03-05 14:37:59 -0800

    Added links and swapped info and about

--- src/components/Pages/AboutSummary.js
+++ src/components/Pages/AboutSummary.js
@@ -1,10 +1,9 @@
 import * as C from './App.styles';
-import {MapChart} from '../Visualizations/CaliforniaCounties'
-import { useData } from '../Visualizations/UseData';
 import "@fontsource/newsreader";
-export const AboutSummary = () => {
 
-  return (
+export const AboutSummary = () => {
+    return(
+      
     <C.Container>
       <C.Area>
         <C.Menu>
@@ -14,49 +13,61 @@ export const AboutSummary = () => {
               alt="logo"
             />
           </div>
-          <div className="menuItem">
-            <ul>
-              <li><span className='active'>Survey Information</span></li>
-              <li><span>About UCCE</span></li>
-              <li><span>Get Involved</span></li>
-            </ul>
-          </div>
         </C.Menu>
 
         <C.Body>
-          <C.Title>UCCE Information</C.Title>
-          <C.Desc className='font-newsreader'>
-                Cooperative Extension (CE) is a nationwide network of land-grant university researchers and educators who solve problems 
-                in agriculture, the environment, and human and community well-being. They work to foster a connection between the university 
-                and the public by delivering science-based information. However, UC Cooperative Extension is facing reductions in personnel, 
-                meaning current advisors are stretched thin and need to prioritize their efforts. At the same time, the landscape of 
-                California agriculture is rapidly changing and farmers are encountering new challenges such as regulations on labor and 
-                inputs. We recently conducted a statewide collaborative needs assessment to give a voice to farmers and others in agriculture
-                to understand their priorities and inform future UCCE programs, increasing engagement and impact. Specifically, we
-                administered an online survey to shed light on common challenges and top concerns of growers, consultants, and allied
-                industry for agronomic crops across the state. Now, our team hopes to disseminate the findings to the general public
-                through an interactive website. This resource will also be critical for state regulatory agencies and policy-makers
-                to identify new opportunities for research, extension, and collaboration.
-            </C.Desc>
-            
-            <C.SubTitle>Survey Scope</C.SubTitle>
+          <C.Title>Who We Are</C.Title>
+          <div class='title-info'>
+            <C.SubTitle>Dr. Cameron Pittelkow</C.SubTitle>
+          </div>
+          <div class='flex-parent'>
+            <div class='flex-child'>
+              <img
+                src="./assets/icons/pittelkow.jpg"
+                alt="logo"
+              />
+            </div>
+            <div class='flex-child'>
+              <C.Desc className='font-newsreader'>
+                  Cameron joined the Department of Plant Sciences in 2019. He is an agronomist who places equal emphasis on food security and 
+                  environmental sustainability. His research is focused on developing cropping systems and management strategies to enhance 
+                  productivity while minimizing nitrogen, carbon, water, and energy footprints in agriculture. Cameron was Assistant Professor 
+                  at the University of Illinois from 2014-2019 before moving to UC Davis. He maintains Adjunct status there while finishing 
+                  projects and advising graduate students until completion of their degrees. He received his BA in Environmental Biology from 
+                  Colgate University, and his MS and PhD degrees from UC Davis in International Agricultural Development and Agronomy, 
+                  respectively.
+              </C.Desc>
+            </div>
+          </div>
+            <C.SubTitle>Development Team</C.SubTitle>
+          <div class='flex-parent'>
+            <div class='flex-child'>
             <C.Desc className='font-newsreader'>
-                    The survey had hundreds of respondants from all over California. There was a large diversity of ages, crop specializations,
-                    and occupations. Vocations interviewed for the survey included growers, consultants, members of allied industries, and others who did not fall into the aforementioned categories.
-                    Below is a map representing the density of responses in respect to county, as well as the distribution of responses per occupation:
-            </C.Desc> 
-            <MapChart data={useData('./data/Filtered_Crop_Data.csv')} filter={"All"} />      
-          <C.Button>
+                    The team that developed this web application is a group of four UC Davis students that were enlisted by Dr. Pittelkow to
+                    create interactive visualizations for the survey findings that could be easily accessed by survey participants. 
+            </C.Desc>
+            </div>
+            <div class='flex-child'>
+            <img
+              src="./assets/icons/us.jpg"
+              alt="logo"
+              width={400}
+              height={200}
+            />
+            </div>
+          </div>    
+          {/* <C.Button>
             <button>
-              See Full Survey results
+              Contact Cameron Pittelkow
               <img 
                 src="/assets/icons/arrow.png"
                 alt="arrow"
               />
             </button>
-          </C.Button>
+          </C.Button> */}
         </C.Body>
       </C.Area>
     </C.Container>
-  );
+    
+    );
 }
