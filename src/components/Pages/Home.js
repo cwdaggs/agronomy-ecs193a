16874c3fe7c0e408f145a61fc01115531759eb34
Akosnikowski <78250440+Akosnikowski@users.noreply.github.com>
2022-03-07 09:29:31 -0800

    Remove home page place holder

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -7,14 +7,14 @@ export const Home = () => {
                 <rect width="800" height="200" x="500" y="100" class="rect-home" />
                 <text class="vis-title" x="850" y="200">Welcome!</text>
 
-                <rect width="400" height="400" x="200" y="400" class="rect-home" />
-                <text class="vis-title" x="330" y="500">Visualizations</text>
+                {/* <rect width="400" height="400" x="200" y="400" class="rect-home" />
+                <text class="vis-title" x="330" y="500">Place Holder</text>
 
                 <rect width="400" height="400" x="700" y="400" class="rect-home" />
-                <text class="vis-title" x="850" y="500">Information</text>
+                <text class="vis-title" x="850" y="500">Place Holder</text>
 
                 <rect width="400" height="400" x="1200" y="400" class="rect-home" />
-                <text class="vis-title" x="1320" y="500">About the Team</text>
+                <text class="vis-title" x="1320" y="500">Place Holder</text> */}
             </svg>
         </div>
     );
