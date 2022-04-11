aea962a73cf229cf11fc20a9414627599a68f203
cmpires <59940851+cmpires@users.noreply.github.com>
2022-04-11 10:36:02 -0700

    Update VisualizationLandingPage.js

--- src/components/Visualizations/Questions/VisualizationLandingPage.js
+++ src/components/Visualizations/Questions/VisualizationLandingPage.js
@@ -62,39 +62,6 @@ export function VisualizationLandingPage(props) {
        window.removeEventListener("scroll", listenToScroll); 
   }, [])
 
-  // disable buttons by adding IDs to them later to find them
-  // think about what to add once stuff is disabled
-  try{
-    var visButton = document.getElementById("visButton");
-    // visButton.style.position = "fixed";
-    // visButton.style.top = "50%";
-    // visButton.style.left = "50%";
-    // visButton.style.transform = "translate(-50%, -50%)";
-    // visButton.style.zIndex = "0";
-
-
-    var buttonDiv = document.getElementById("compareDiv");
-    //buttonDiv.style.display = "none";
-
-    //var visButton = document.getElementById("visButton");
-    // visButton.style.marginLeft = "47%";
-    // visButton.style.marginTop = "50px";
-    // visButton.style.marginBottom = "50px";
-
-    var visWelcomePage = document.getElementById("visWelcome");
-    //visWelcomePage.style.backgroundImage = "url(./images/pexels-seb-360013.jpg)"
-  //     background-image: url(./images/pexels-seb-360013.jpg);
-  // /* Need to make height dynamic */
-  // height: 800px;
-  // background-repeat: no-repeat;
-  // background-attachment: fixed; 
-  // background-size: 100% 100%;
-  } catch (error) {
-    // compare button doesn't exist yet
-    // TODO: add function to wait for it to exist before trying to hide it
-  }
-  
-
   return (
     <div>
       <div id="visLandingPage">
