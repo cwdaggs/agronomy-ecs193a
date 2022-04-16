df2fd9f815277ff2dc338d33773bb7ba6265d26c
cmpires <59940851+cmpires@users.noreply.github.com>
2022-04-15 20:52:36 -0700

    initial info scrolling page (needs new background pictures and formatting)

--- src/components/Pages/InfoSummary.js
+++ src/components/Pages/InfoSummary.js
@@ -3,65 +3,94 @@ import {MapChart, CropBar} from '../Visualizations/CaliforniaCounties'
 import {RegionMapChart} from '../Visualizations/RegionMap'
 import { useData } from '../Visualizations/UseData';
 import "@fontsource/newsreader";
-import file from './downloads/ResearchPaper.pdf'
+import file from './downloads/ResearchPaper.pdf';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import wheat from '../../images/wheat_field.png'
+
+
 export const InfoSummary = () => {
 
   return (
-    <C.Container>
-      <C.Area>
-        <C.Menu>
-          <div >
-            
-          </div>
-          <div className="menuItem">
-            <ul>
-              <li><a href={file} download>Survey Research Paper</a></li>
-              <li><a href='https://caes.ucdavis.edu/outreach/ce'>About UCCE</a> </li>
-              <li><a href='https://caes.ucdavis.edu/outreach/geo/projects'>Get Involved</a></li>
-            </ul>
-          </div>
-        </C.Menu>
+    <div>
+      <Parallax pages={4}>
+        <C.Container>
+          <C.Area>
+            <C.Menu>
+              <div >
+                
+              </div>
+              <div className="menuItem">
+                <ul>
+                  <li><a href={file} download>Survey Research Paper</a></li>
+                  <li><a href='https://caes.ucdavis.edu/outreach/ce'>About UCCE</a> </li>
+                  <li><a href='https://caes.ucdavis.edu/outreach/geo/projects'>Get Involved</a></li>
+                </ul>
+              </div>
+            </C.Menu>
 
-        <C.Body>
-          <C.Title>UCCE Information</C.Title>
-          <C.Desc className='font-newsreader'>
-                Cooperative Extension (CE) is a nationwide network of land-grant university researchers and educators who solve problems 
-                in agriculture, the environment, and human and community well-being. They work to foster a connection between the university 
-                and the public by delivering science-based information. However, UC Cooperative Extension is facing reductions in personnel, 
-                meaning current advisors are stretched thin and need to prioritize their efforts. At the same time, the landscape of 
-                California agriculture is rapidly changing and farmers are encountering new challenges such as regulations on labor and 
-                inputs. We recently conducted a statewide collaborative needs assessment to give a voice to farmers and others in agriculture
-                to understand their priorities and inform future UCCE programs, increasing engagement and impact. Specifically, we
-                administered an online survey to shed light on common challenges and top concerns of growers, consultants, and allied
-                industry for agronomic crops across the state. Now, our team hopes to disseminate the findings to the general public
-                through an interactive website. This resource will also be critical for state regulatory agencies and policy-makers
-                to identify new opportunities for research, extension, and collaboration.
-            </C.Desc>
-            
-            <C.SubTitle>Survey Scope</C.SubTitle>
-            <C.Desc className='font-newsreader'>
-                    The survey had hundreds of respondants from all over California. There was a large diversity of ages, crop specializations,
-                    and occupations. Vocations interviewed for the survey included growers, consultants, members of allied industries, and others who did not fall into the aforementioned categories.
-                    Below is a map representing the density of responses in respect to county, as well as the distribution of responses per occupation:
-            </C.Desc> 
-            <MapChart data={useData('./data/Filtered_Crop_Data.csv')} filter={"All"} />
-            <RegionMapChart data={useData('./data/Filtered_Crop_Data.csv')} filter={"All"} />
-            <C.Desc className='font-newsreader'>
-                    Additionally, survey participants could list their top grown or consulted crops. Below is a bar chart depicting the number of
-                    responses for each crop:
-            </C.Desc> 
-            <CropBar data={useData('./data/Filtered_Crop_Data.csv')}/>
-          {/* <C.Button>
-            <button>
-              See Full Survey results
-              <img 
-                src="/assets/icons/arrow.png"
-                alt="arrow"
-              />
-            </button>
-          </C.Button> */}
-        </C.Body>
-      </C.Area>
-    </C.Container>
+            <C.Body>
+              <ParallaxLayer factor={1} style={{
+                backgroundImage: `url(${wheat})`,
+                backgroundSize: 'cover'
+              }}>
+              <div id = "infoSection1">
+              <C.Title>UCCE Information</C.Title>
+              <C.Desc className='font-newsreader'>
+                    Cooperative Extension (CE) is a nationwide network of land-grant university researchers and educators who solve problems 
+                    in agriculture, the environment, and human and community well-being. They work to foster a connection between the university 
+                    and the public by delivering science-based information. However, UC Cooperative Extension is facing reductions in personnel, 
+                    meaning current advisors are stretched thin and need to prioritize their efforts. At the same time, the landscape of 
+                    California agriculture is rapidly changing and farmers are encountering new challenges such as regulations on labor and 
+                    inputs. We recently conducted a statewide collaborative needs assessment to give a voice to farmers and others in agriculture
+                    to understand their priorities and inform future UCCE programs, increasing engagement and impact. Specifically, we
+                    administered an online survey to shed light on common challenges and top concerns of growers, consultants, and allied
+                    industry for agronomic crops across the state. Now, our team hopes to disseminate the findings to the general public
+                    through an interactive website. This resource will also be critical for state regulatory agencies and policy-makers
+                    to identify new opportunities for research, extension, and collaboration.
+                </C.Desc>
+                
+                <C.SubTitle>Survey Scope</C.SubTitle>
+                <C.Desc className='font-newsreader'>
+                        The survey had hundreds of respondants from all over California. There was a large diversity of ages, crop specializations,
+                        and occupations. Vocations interviewed for the survey included growers, consultants, members of allied industries, and others who did not fall into the aforementioned categories.
+                        Below is a map representing the density of responses in respect to county, as well as the distribution of responses per occupation:
+                </C.Desc>
+                </div>
+                </ParallaxLayer>
+                <ParallaxLayer factor={2} offset={1} style={{
+                backgroundImage: `url(${wheat})`,
+                backgroundSize: 'cover'
+              }}> 
+                <div id = "infoSection2">
+                <MapChart data={useData('./data/Filtered_Crop_Data.csv')} filter={"All"} />
+                <RegionMapChart data={useData('./data/Filtered_Crop_Data.csv')} filter={"All"} />
+                </div>
+                </ParallaxLayer>
+                <ParallaxLayer factor={1} offset={3} style={{
+                backgroundImage: `url(${wheat})`,
+                backgroundSize: 'cover'
+              }}> 
+                <div id = "infoSection3">
+                <C.Desc className='font-newsreader' style={{textAlign: 'center'}}>
+                        <br></br><br></br>Additionally, survey participants could list their top grown or consulted crops. Below is a bar chart depicting the number of
+                        responses for each crop:
+                </C.Desc> 
+                <CropBar data={useData('./data/Filtered_Crop_Data.csv')}/>
+                </div>
+                </ParallaxLayer>
+              {/* <C.Button>
+                <button>
+                  See Full Survey results
+                  <img 
+                    src="/assets/icons/arrow.png"
+                    alt="arrow"
+                  />
+                </button>
+              </C.Button> */}
+            </C.Body>
+          </C.Area>
+        </C.Container>
+      </Parallax>
+    </div>
   );
 }
