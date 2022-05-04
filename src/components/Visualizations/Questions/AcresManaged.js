ac299d993cf44278a291ec0d9ed42ac5e3cc1cf2
cwdaggs <wmanu2013@gmail.com>
2022-05-03 17:19:40 -0700

    Future for pie chart scaling

--- src/components/Visualizations/Questions/AcresManaged.js
+++ src/components/Visualizations/Questions/AcresManaged.js
@@ -57,31 +57,6 @@ function calculateSizeOfDataSet(data){
   return size;
 }
 
-// function parseURL(baseURL, path) {
-//   var pathname = path;
-//   var vocation = "All";
-//   var cropOrRegion = "All";
-//   if (baseURL !== pathname) {
-//     pathname = pathname.replace(baseURL, "");
-//     const filters = pathname.split("/");
-//     filters.shift();
-//     // console.log(filters);
-//     if (filters[0] !== "Select%20Vocation") {
-//       vocation = filters[0];
-//     }
-    
-//     if (filters[1] === "Select%20Crop" && filters[2] === "Select%20Region") {
-//       cropOrRegion = "All";
-//     } else if (filters[1] === "Select%20Crop"){
-//       cropOrRegion = filters[2].replaceAll("%20", " ");
-//     } else {
-//       cropOrRegion = filters[1].replaceAll("%20", " ");
-//     }
-//   } 
-//   // console.log("vocation: " + vocation + ", croporreg: " + cropOrRegion);
-//   return {vocation: vocation, cropOrRegion: cropOrRegion};
-// }
-
 export function AcresManagedBarChart(props) {
     const vocationArray = ["All", "Growers", "Consultants"];
     const baseURL = "/results/Acres%20Managed";
