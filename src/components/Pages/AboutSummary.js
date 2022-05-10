69b73332eab1f6bd7b9263bde0f7b991a4f50652
cwdaggs <wmanu2013@gmail.com>
2022-05-10 09:16:16 -0700

    Redid team page

--- src/components/Pages/AboutSummary.js
+++ src/components/Pages/AboutSummary.js
@@ -10,88 +10,88 @@ export const AboutSummary = () => {
     <C.Container>
       <C.Area>
         <C.Body>
-          <C.Title>Project Team</C.Title>
+          <C.ProfileTitle>Project Team</C.ProfileTitle>
           <br></br>
           <div id='team-profiles'>
             <div className='profile'>
-              <img src="./assets/1-Jessie-Kanter-UCCE.jpg" alt="Jessie Kanter" height={height/3} width={height/3}/>
+              <img src="./assets/1-Jessie-Kanter-UCCE.jpg" alt="Jessie Kanter" className="profile-img"/>
               <div className='profile-name'>
                 <span>Jessie Kanter - </span>
                 <a href='https://ucanr.edu/About/DirectorySearch/?facultyid=45966' target="_blank">UCCE</a>
               </div>
             </div>
             <div className='profile'>
-              <img src="./assets/2-Nick-Clark-UCCE.jpg" alt="Nick Clark" height={height/3} width={height/3}/>
+              <img src="./assets/2-Nick-Clark-UCCE.jpg" alt="Nick Clark" className="profile-img"/>
               <div className='profile-name'>
                 <span>Nick Clark - </span>
                 <a href='https://ucanr.edu/sites/Bees_and_Pollination/Contact_Us/?facultyid=26947' target="_blank">UCCE</a>
               </div>
             </div>
             <div className='profile'>
-              <img src="./assets/3-Mark-Lundy-Davis.jpg" alt="Mark Lundy" height={height/3} width={height/3}/>
+              <img src="./assets/3-Mark-Lundy-Davis.jpg" alt="Mark Lundy" className="profile-img"/>
               <div className='profile-name'>
                 <span>Mark Lundy - </span>
                 <a href='https://www.plantsciences.ucdavis.edu/people/mark-lundy' target="_blank">UC Davis</a>
               </div>
             </div>
             <div className='profile'>
-              <img src="./assets/4-Vikram-Koundinya-Davis.jpg" alt="Vikram Koundinya" height={height/3} width={height/3}/>
+              <img src="./assets/4-Vikram-Koundinya-Davis.jpg" alt="Vikram Koundinya" className="profile-img"/>
               <div className='profile-name'>
                 <span>Vikram Koundinya - </span>
                 <a href='https://humanecology.ucdavis.edu/people/vikram-koundinya' target="_blank">UC Davis</a>
               </div>
             </div>
             <div className='profile'>
-              <img src="./assets/5-Michelle-Leinfelder-Miles-UCCE.jpg" alt="Michelle Leinfelder-Miles" height={height/3} width={height/3}/>
+              <img src="./assets/5-Michelle-Leinfelder-Miles-UCCE.jpg" alt="Michelle Leinfelder-Miles" className="profile-img"/>
               <div className='profile-name'>
                 <span>Michelle Leinfelder-Miles - </span>
                 <a href='https://ucanr.edu/?facultyid=20507' target="_blank">UCCE</a>
               </div>
             </div>
             <div className='profile'>
-              <img src="./assets/6-Rachael-Long-UCCE.jpg" alt="Rachael Long" height={height/3} width={height/3}/>
+              <img src="./assets/6-Rachael-Long-UCCE.jpg" alt="Rachael Long" className="profile-img"/>
               <div className='profile-name'>
                 <span>Rachael Long - </span>
                 <a href='https://ucanr.edu/?facultyid=1355' target="_blank">UCCE</a>
               </div>
             </div>
             <div className='profile'>
-              <img src="./assets/7-Sarah-Light-UCCE.jpg" alt="Sarah Light" height={height/3} width={height/3}/>
+              <img src="./assets/7-Sarah-Light-UCCE.jpg" alt="Sarah Light" className="profile-img"/>
               <div className='profile-name'>
                 <span>Sarah Light - </span>
                 <a href='https://cesutter.ucanr.edu/http___cesutterucanredu/?facultyid=36230' target="_blank">UCCE</a>
               </div>
             </div>
             <div className='profile'>
-              <img src="./assets/8-Whitney-Brim-DeForest-UCCE.jpg" alt="Whitney Brim-DeForest" height={height/3} width={height/3}/>
+              <img src="./assets/8-Whitney-Brim-DeForest-UCCE.jpg" alt="Whitney Brim-DeForest" className="profile-img"/>
               <div className='profile-name'>
                 <span>Whitney Brim-DeForest - </span>
                 <a href='https://ucanr.edu/?facultyid=33566' target="_blank">UCCE</a>
               </div>
             </div>
             <div className='profile'>
-              <img src="./assets/9-Bruce-Linquist-Davis.jpg" alt="Bruce Linquist" height={height/3} width={height/3}/>
+              <img src="./assets/9-Bruce-Linquist-Davis.jpg" alt="Bruce Linquist" className="profile-img"/>
               <div className='profile-name'>
                 <span>Bruce Linquist - </span>
                 <a href='https://www.plantsciences.ucdavis.edu/people/bruce-linquist' target="_blank">UC Davis</a>
               </div>
             </div>
             <div className='profile'>
-              <img src="./assets/10-Dan-Putnam-Davis.jpg" alt="Dan Putnam" height={height/3} width={height/3}/>
+              <img src="./assets/10-Dan-Putnam-Davis.jpg" alt="Dan Putnam" className="profile-img"/>
               <div className='profile-name'>
                 <span>Dan Putnam - </span>
                 <a href='https://www.plantsciences.ucdavis.edu/people/daniel-putnam' target="_blank">UC Davis</a>
               </div>
             </div>
             <div className='profile'>
-              <img src="./assets/11-Robert-B.-Hutmacher-Davis.jpg" alt="Robert B. Hutmacher" height={height/3} width={height/3}/>
+              <img src="./assets/11-Robert-B.-Hutmacher-Davis.jpg" alt="Robert B. Hutmacher" className="profile-img"/>
               <div className='profile-name'>
                 <span>Robert B. Hutmacher - </span>
                 <a href='https://www.plantsciences.ucdavis.edu/people/robert-hutmacher' target="_blank">UC Davis</a>
               </div>
             </div>
             <div className='profile'>
-              <img src="./assets/12-Cameron-Pittelkow-Davis.jpg" alt="Cameron Pittelkow" height={height/3} width={height/3}/>
+              <img src="./assets/12-Cameron-Pittelkow-Davis.jpg" alt="Cameron Pittelkow" className="profile-img"/>
               <div className='profile-name'>
                 <span>Cameron Pittelkow - </span>
                 <a href='https://www.plantsciences.ucdavis.edu/people/cameron-pittelkow' target="_blank">UC Davis</a>
@@ -101,32 +101,32 @@ export const AboutSummary = () => {
         </C.Body>
 
         <C.Body>
-          <C.Title>Application Development Team</C.Title>
+          <C.ProfileTitle>Application Team</C.ProfileTitle>
           <br></br>
           <div id='team-profiles'>
             <div className='profile'>
-              <img src="./assets/Austin-Kosnikowski.jpg" alt="Austin Kosnikowski" height={height/3} width={height/3}/>
+              <img src="./assets/Austin-Kosnikowski.jpg" alt="Austin Kosnikowski" className="profile-img"/>
               <div className='profile-name'>
                 <span>Austin Kosnikowski - </span>
                 <a href='https://www.linkedin.com/in/austin-kosnikowski-769206236/' target="_blank">LinkedIn</a>
               </div>
             </div>
             <div className='profile'>
-              <img src="./assets/Cameron-Daggs.jpg" alt="Cameron Daggs" height={height/3} width={height/3}/>
+              <img src="./assets/Cameron-Daggs.jpg" alt="Cameron Daggs" className="profile-img"/>
               <div className='profile-name'>
                 <span>Cameron Daggs - </span>
                 <a href='https://www.linkedin.com/in/cameron-daggs-45a2a6163/' target="_blank">LinkedIn</a>
               </div>
             </div>
             <div className='profile'>
-              <img src="./assets/Christopher-Pires.jpg" alt="Christopher Pires" height={height/3} width={height/3}/>
+              <img src="./assets/Christopher-Pires.jpg" alt="Christopher Pires" className="profile-img"/>
               <div className='profile-name'>
                 <span>Christopher Pires - </span>
                 <a href='https://www.linkedin.com/in/christopher-pires-0280ba20b/' target="_blank">LinkedIn</a>
               </div>
             </div>
             <div className='profile'>
-              <img src="./assets/Darin-Lee.jpg" alt="Darin Lee" height={height/3} width={height/3}/>
+              <img src="./assets/Darin-Lee.jpg" alt="Darin Lee" className="profile-img"/>
               <div className='profile-name'>
                 <span>Darin Lee - </span>
                 <a href='https://www.linkedin.com/in/darin-lee-3486031b1/' target="_blank">LinkedIn</a>
