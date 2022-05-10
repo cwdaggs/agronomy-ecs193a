0a0ee6327179645f9d8f9f4688d15b9c71e74036
cwdaggs <wmanu2013@gmail.com>
2022-05-10 09:15:38 -0700

    Update VisualizationLandingPage.js

--- src/components/Visualizations/Questions/VisualizationLandingPage.js
+++ src/components/Visualizations/Questions/VisualizationLandingPage.js
@@ -1,7 +1,7 @@
 export function VisualizationLandingPage(props) {
 
   return (
-    <div id="visLandingPage">
+    <div class="visLandingPage">
         <h1 id="visLandingHeading">Welcome!</h1>
         {<p id="visLandingBody">- By exploring these results, you can discover how grower's and consultant's preferences and decisions
         change depending on factors such as their region and choice of crop. <br></br> <br></br> - Additionally, you can compare how your priorities and concerns align with other participants.
