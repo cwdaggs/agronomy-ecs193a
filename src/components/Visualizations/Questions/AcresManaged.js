722d88841b6d8fde995c16446343f050107ab2d9
Akosnikowski <78250440+Akosnikowski@users.noreply.github.com>
2022-05-18 15:26:06 -0700

    url fixes for all compares and acres managed

--- src/components/Visualizations/Questions/AcresManaged.js
+++ src/components/Visualizations/Questions/AcresManaged.js
@@ -1,5 +1,5 @@
 import {VictoryAxis, VictoryChart, VictoryBar, VictoryTooltip} from 'victory';
-import {filterByCropOrRegion, filterByVocation} from '../UseData.js';
+import {filterByCropOrRegion, filterByVocation, filterByCrop, filterByRegion} from '../UseData.js';
 import { VocationAndRegion, VocationAndRegionCompare } from "../Menus/VocationAndRegion.js";
 import {useState} from "react";
 import { useLocation } from 'react-router-dom';
@@ -128,33 +128,41 @@ export function AcresManagedBarChart(props) {
     const vocationArray = ["All", "Growers", "Consultants"];
     const baseURL = "/results/Acres%20Managed";
     const filters = parseURL(baseURL, useLocation().pathname, vocationArray);
+
     const [activeVocation, setActiveVocation] = useState(filters.vocation);
-    const [activeRegionOrCrop, setActiveRegionOrCrop] = useState(filters.cropOrRegion);
+    const [activeRegion, setActiveRegion] = useState(filters.region);
+    const [activeCrop, setActiveCrop] = useState(filters.crop)
+  
 
     function vocationFunction(newValue){
       setActiveVocation(newValue);
     }
-
-    function regionOrCropFunction(newValue) {
-      setActiveRegionOrCrop(newValue);
+  
+    function regionFunction(newValue) {
+      setActiveRegion(newValue);
     }
+  
+    function cropFunction(newValue) {
+      setActiveCrop(newValue)
+    }
+
 
     if (!props.dataset) {
-        return <pre>Loading...</pre>;
+      return <pre>Loading...</pre>;
     }
-    var data = filterByVocation(filterByCropOrRegion(props.dataset, activeRegionOrCrop), activeVocation);
+    var data = filterByVocation(filterByRegion(filterByCrop(props.dataset, activeCrop), activeRegion), activeVocation);
     var acre_data = calculateAcres(data);
     var dataLength = calculateSizeOfDataSet(acre_data)
     var lengthString = String("Number of Farms (n = " + dataLength + ")");
     
     var labelText = "Acres";
     if (activeVocation === "Growers") {
-      labelText = "Acres Managed by " + activeRegionOrCrop + " Growers";
+      labelText = "Acres Managed by " + activeRegion + " " + activeCrop + " Growers";
     } else if (activeVocation === "Consultants") {
-      labelText = "Acres Consulted by " + activeRegionOrCrop + " Consultants";
+      labelText = "Acres Consulted by " + activeRegion + " " + activeCrop + " Consultants";
     } else {
-      if (activeRegionOrCrop !== "All") {
-        labelText = activeRegionOrCrop + " " + labelText;
+      if (activeCrop !== "All") {
+        labelText = activeRegion + " " + activeCrop + " " + labelText;
       }
     }
     
@@ -172,7 +180,7 @@ export function AcresManagedBarChart(props) {
             <h2>How many acres do you manage/consult annually?</h2>
         </div>
         <div className="inline-child">
-          <VocationAndRegion vocationFunction={vocationFunction} regionOrCropFunction={regionOrCropFunction} activeVocation={activeVocation} activeRegionOrCrop={activeRegionOrCrop} vocationArray={vocationArray} baseAll={filters.baseAll}/>
+          <VocationAndRegion vocationFunction={vocationFunction} regionFunction={regionFunction} cropFunction={cropFunction} activeVocation={activeVocation} activeRegion={activeRegion} activeCrop={activeCrop} vocationArray={vocationArray} baseAll={filters.baseAll}/>
         </div>
         <GetChart height={height} width={width} margin={margin} fontSize={fontSize} mobileWidth={mobileWidth} acre_data={acre_data} labelText={labelText} lengthString={lengthString} vocationArray={vocationArray} filters={filters} visIndex={1}/>
           
@@ -185,58 +193,71 @@ export function AcresManagedBarChartCompare(props) {
   const baseURL = "/results/compare/Acres%20Managed";
   const filters = parseURLCompare(baseURL, useLocation().pathname, vocationArray);
   const [activeVocation, setActiveVocation] = useState(filters.vocation);
-  const [activeRegionOrCrop, setActiveRegionOrCrop] = useState(filters.cropOrRegion);
+  const [activeRegion, setActiveRegion] = useState(filters.region);
+  const [activeCrop, setActiveCrop] = useState(filters.crop)
+
   const [activeVocation2, setActiveVocation2] = useState(filters.vocation2);
-  const [activeRegionOrCrop2, setActiveRegionOrCrop2] = useState(filters.cropOrRegion2);
+  const [activeRegion2, setActiveRegion2] = useState(filters.region2);
+  const [activeCrop2, setActiveCrop2] = useState(filters.crop2)
 
   function vocationFunction(newValue){
     setActiveVocation(newValue);
   }
 
-  function regionOrCropFunction(newValue) {
-    setActiveRegionOrCrop(newValue);
+  function regionFunction(newValue) {
+    setActiveRegion(newValue);
+  }
+
+  function cropFunction(newValue) {
+    setActiveCrop(newValue)
   }
 
   function vocationFunction2(newValue){
     setActiveVocation2(newValue);
   }
 
-  function regionOrCropFunction2(newValue) {
-    setActiveRegionOrCrop2(newValue);
+  function regionFunction2(newValue) {
+    setActiveRegion2(newValue);
   }
-  console.log(filters)
+
+  function cropFunction2(newValue) {
+    setActiveCrop2(newValue)
+  }
+
+
+  //console.log(filters)
   if (!props.dataset) {
       return <pre>Loading...</pre>;
   }
-  var data = filterByVocation(filterByCropOrRegion(props.dataset, activeRegionOrCrop), activeVocation);
+  var data = filterByVocation(filterByRegion(filterByCrop(props.dataset, activeCrop), activeRegion), activeVocation);
   var acre_data = calculateAcres(data);
   var dataLength = calculateSizeOfDataSet(acre_data)
   var lengthString = String("Number of Farms (n = " + dataLength + ")");
 
-  var data2 = filterByVocation(filterByCropOrRegion(props.dataset, activeRegionOrCrop2), activeVocation2);
+  var data2 = filterByVocation(filterByRegion(filterByCrop(props.dataset, activeCrop2), activeRegion2), activeVocation2);
   var acre_data2 = calculateAcres(data2);
   var dataLength2 = calculateSizeOfDataSet(acre_data2)
   var lengthString2 = String("Number of Farms (n = " + dataLength2 + ")");
   
   var labelText = "Acres";
   if (activeVocation === "Growers") {
-    labelText = "Acres Managed by " + activeRegionOrCrop + " Growers";
+    labelText = "Acres Managed by " + activeRegion + " " + activeCrop + " Growers";
   } else if (activeVocation === "Consultants") {
-    labelText = "Acres Consulted by " + activeRegionOrCrop + " Consultants";
+    labelText = "Acres Consulted by " + activeRegion + " " + activeCrop + " Consultants";
   } else {
-    if (activeRegionOrCrop !== "All") {
-      labelText = activeRegionOrCrop + " " + labelText;
+    if (activeCrop !== "All") {
+      labelText = activeRegion + " " + activeCrop + " " + labelText;
     }
   }
 
   var labelText2 = "Acres";
   if (activeVocation2 === "Growers") {
-    labelText2 = "Acres Managed by " + activeRegionOrCrop2 + " Growers";
+    labelText2 = "Acres Managed by " + activeRegion2 + " " + activeCrop2 + " Growers";
   } else if (activeVocation2 === "Consultants") {
-    labelText2 = "Acres Consulted by " + activeRegionOrCrop2 + " Consultants";
+    labelText2 = "Acres Consulted by " + activeRegion2 + " " + activeCrop2 + " Consultants";
   } else {
-    if (activeRegionOrCrop2 !== "All") {
-      labelText2 = activeRegionOrCrop2 + " " + labelText2;
+    if (activeCrop2 !== "All") {
+      labelText2 = activeRegion2 + " " + activeCrop2 + " " + labelText2;
     }
   }
   
@@ -254,7 +275,7 @@ export function AcresManagedBarChartCompare(props) {
           <h2>How many acres do you manage/consult annually?</h2>
       </div>
       <div className="inline-child">
-          <VocationAndRegionCompare vocationFunction={vocationFunction} regionOrCropFunction={regionOrCropFunction} activeVocation={activeVocation} activeRegionOrCrop={activeRegionOrCrop} vocationFunction2={vocationFunction2} regionOrCropFunction2={regionOrCropFunction2} activeVocation2={activeVocation2} activeRegionOrCrop2={activeRegionOrCrop2} vocationArray={vocationArray} baseAll={filters.baseAll}/>
+          <VocationAndRegionCompare vocationFunction={vocationFunction} regionFunction={regionFunction} cropFunction={cropFunction} activeVocation={activeVocation} activeRegion={activeRegion} activeCrop={activeCrop} vocationFunction2={vocationFunction2} regionFunction2={regionFunction2} cropFunction2={cropFunction2} activeVocation2={activeVocation2} activeCrop2={activeCrop2} activeRegion2={activeRegion2} vocationArray={vocationArray} baseAll={filters.baseAll}/>
         </div>
       <div className='dual-display'>
         <div id='dual-display-child'>
--- src/components/Visualizations/UseData.js
+++ src/components/Visualizations/UseData.js
@@ -130,6 +130,31 @@ export function filterByCropOrRegion(data, filter){
   }
 }
 
+export function filterByRegion(data, filter){
+  const regionTypes = ["Intermountain", "Sac Valley", "NSJV", "SSJV", "Desert", "Coastal", "Sierra Nevada"];
+  if(filter === "All"){
+    return data
+  } 
+  else if(regionTypes.includes(filter)){
+    if(filter === "Sac Valley"){
+      filter = "Sac_Valley";
+    }
+    else if(filter === "Sierra Nevada"){
+      filter = "Sierra_Nevada";
+    }
+  }
+  return data.filter(function(d){return String(d.Region).includes(filter)});
+}
+
+export function filterByCrop(data, filter){
+    
+    if(filter === "All"){
+      return data
+    }else{
+      return data.filter(function(d){return String(d.Crops).includes(filter)});
+    }
+}
+
 export function filterByVocation(data, filter){
   switch(filter){
     case "All":
@@ -394,10 +419,56 @@ export function acresByCounty(data){
   return county_acres
 }
 
+// export function parseURL(baseURL, path, vocationArray) {
+//   var pathname = path;
+//   var vocation = vocationArray[0];
+//   var cropOrRegion = "All";
+//   var baseAll = true;
+//   const cropChoices = [
+//     "All", 
+//     "Alfalfa", 
+//     "Barley", 
+//     "Corn", 
+//     "Corn Silage", 
+//     "Cotton", 
+//     "Dry Beans", 
+//     "Rice", 
+//     "Small Grain Silage", 
+//     "Sunflower", 
+//     "Wheat"
+//   ];
+
+//   const regionChoices = ["All", "Intermountain", "Sac Valley", "NSJV", "SSJV", "Desert", "Coastal", "Sierra Nevada"];
+
+//   if (baseURL !== pathname) {
+//     pathname = pathname.replace(baseURL, "");
+//     const filters = pathname.split("/");
+//     filters.shift();
+//     // console.log(filters);
+//     if (filters[0] !== "Select%20Vocation" && vocationArray.includes(filters[0])) {
+//       vocation = filters[0];
+//       baseAll = false;
+//     }
+    
+//     if (filters[1] === "Select%20Crop" && filters[2] === "Select%20Region") {
+//       cropOrRegion = "All";
+//     } else if (filters[1] === "Select%20Crop" && regionChoices.includes(filters[2])){
+//       cropOrRegion = filters[2].replaceAll("%20", " ");
+//       baseAll = false;
+//     } else if (filters[2] === "Select%20Region" && cropChoices.includes(filters[1])){
+//       cropOrRegion = filters[1].replaceAll("%20", " ");
+//       baseAll = false;
+//     }
+//   } 
+//   // console.log("vocation: " + vocation + ", croporreg: " + cropOrRegion);
+//   return {vocation: vocation, cropOrRegion: cropOrRegion, baseAll: baseAll};
+// }
+
 export function parseURL(baseURL, path, vocationArray) {
   var pathname = path;
   var vocation = vocationArray[0];
-  var cropOrRegion = "All";
+  var crop = "All";
+  var region = "All";
   var baseAll = true;
   const cropChoices = [
     "All", 
@@ -426,17 +497,22 @@ export function parseURL(baseURL, path, vocationArray) {
     }
     
     if (filters[1] === "Select%20Crop" && filters[2] === "Select%20Region") {
-      cropOrRegion = "All";
+      crop = "All";
+      region = "All"
     } else if (filters[1] === "Select%20Crop" && regionChoices.includes(filters[2])){
-      cropOrRegion = filters[2].replaceAll("%20", " ");
+      region = filters[2].replaceAll("%20", " ");
       baseAll = false;
     } else if (filters[2] === "Select%20Region" && cropChoices.includes(filters[1])){
-      cropOrRegion = filters[1].replaceAll("%20", " ");
+      crop = filters[1].replaceAll("%20", " ");
+      baseAll = false;
+    } else{
+      region = filters[2].replaceAll("%20", " ");
+      crop = filters[1].replaceAll("%20", " ");
       baseAll = false;
     }
   } 
   // console.log("vocation: " + vocation + ", croporreg: " + cropOrRegion);
-  return {vocation: vocation, cropOrRegion: cropOrRegion, baseAll: baseAll};
+  return {vocation: vocation, crop: crop, region: region, baseAll: baseAll};
 }
 
 export function parseCropURLCompare(baseURL, path){
@@ -477,12 +553,77 @@ export function parseCropURLCompare(baseURL, path){
   return {crop1:crop1, crop2:crop2, baseAll: baseAll};
 }
 
+// export function parseURLCompare(baseURL, path, vocationArray) {
+//   var pathname = path;
+//   var vocation = vocationArray[0];
+//   var cropOrRegion = "All";
+//   var vocation2 = vocationArray[0];
+//   var cropOrRegion2 = "All";
+//   var baseAll = true;
+
+//   const cropChoices = [
+//     "All", 
+//     "Alfalfa", 
+//     "Barley", 
+//     "Corn", 
+//     "Corn Silage", 
+//     "Cotton", 
+//     "Dry Beans", 
+//     "Rice", 
+//     "Small Grain Silage", 
+//     "Sunflower", 
+//     "Wheat"
+//   ];
+
+//   const regionChoices = ["All", "Intermountain", "Sac Valley", "NSJV", "SSJV", "Desert", "Coastal", "Sierra Nevada"];
+
+//   if (baseURL !== pathname) {
+//     pathname = pathname.replace(baseURL, "");
+//     const filters = pathname.split("/");
+//     filters.shift();
+//     // console.log(filters);
+//     if (filters[0] !== "Select%20Vocation" && vocationArray.includes(filters[0])) {
+//       vocation = filters[0];
+//       baseAll = false;
+//     }
+    
+//     if (filters[1] === "Select%20Crop" && filters[2] === "Select%20Region") {
+//       cropOrRegion = "All";
+//     } else if (filters[1] === "Select%20Crop" && regionChoices.includes(filters[2])){
+//       cropOrRegion = filters[2].replaceAll("%20", " ");
+//       baseAll = false;
+//     } else if (filters[2] === "Select%20Region" && cropChoices.includes(filters[1])){
+//       cropOrRegion = filters[1].replaceAll("%20", " ");
+//       baseAll = false;
+//     }
+
+//     if (filters[3] !== "Compare%20Vocation" && vocationArray.includes(filters[3])) {
+//       vocation2 = filters[3];
+//       baseAll = false;
+//     }
+    
+//     if (filters[4] === "Compare%20Crop" && filters[5] === "Compare%20Region") {
+//       cropOrRegion2 = "All";
+//     } else if (filters[4] === "Compare%20Crop"  && vocationArray.includes(filters[5])){
+//       cropOrRegion2 = filters[5].replaceAll("%20", " ");
+//       baseAll = false;
+//     } else if (filters[2] === "Select%20Region" && cropChoices.includes(filters[4])){
+//       cropOrRegion2 = filters[4].replaceAll("%20", " ");
+//       baseAll = false;
+//     }
+//   } 
+//   // console.log("vocation: " + vocation + ", croporreg: " + cropOrRegion);
+//   return {vocation: vocation, cropOrRegion: cropOrRegion, vocation2:vocation2, cropOrRegion2:cropOrRegion2, baseAll: baseAll};
+// }
+
 export function parseURLCompare(baseURL, path, vocationArray) {
   var pathname = path;
   var vocation = vocationArray[0];
-  var cropOrRegion = "All";
+  var crop = "All";
+  var region = "All";
   var vocation2 = vocationArray[0];
-  var cropOrRegion2 = "All";
+  var crop2 = "All";
+  var region2 = "All"
   var baseAll = true;
 
   const cropChoices = [
@@ -512,12 +653,17 @@ export function parseURLCompare(baseURL, path, vocationArray) {
     }
     
     if (filters[1] === "Select%20Crop" && filters[2] === "Select%20Region") {
-      cropOrRegion = "All";
+      crop = "All";
+      region = "All"
     } else if (filters[1] === "Select%20Crop" && regionChoices.includes(filters[2])){
-      cropOrRegion = filters[2].replaceAll("%20", " ");
+      region = filters[2].replaceAll("%20", " ");
       baseAll = false;
     } else if (filters[2] === "Select%20Region" && cropChoices.includes(filters[1])){
-      cropOrRegion = filters[1].replaceAll("%20", " ");
+      crop = filters[1].replaceAll("%20", " ");
+      baseAll = false;
+    } else{
+      region = filters[2].replaceAll("%20", " ");
+      crop = filters[1].replaceAll("%20", " ");
       baseAll = false;
     }
 
@@ -527,17 +673,22 @@ export function parseURLCompare(baseURL, path, vocationArray) {
     }
     
     if (filters[4] === "Compare%20Crop" && filters[5] === "Compare%20Region") {
-      cropOrRegion2 = "All";
-    } else if (filters[4] === "Compare%20Crop"  && vocationArray.includes(filters[5])){
-      cropOrRegion2 = filters[5].replaceAll("%20", " ");
+      crop2 = "All";
+      region2 = "All"
+    } else if (filters[4] === "Compare%20Crop" && regionChoices.includes(filters[5])){
+      region2 = filters[5].replaceAll("%20", " ");
       baseAll = false;
-    } else if (filters[2] === "Select%20Region" && cropChoices.includes(filters[4])){
-      cropOrRegion2 = filters[4].replaceAll("%20", " ");
+    } else if (filters[5] === "Compare%20Region" && cropChoices.includes(filters[4])){
+      crop2 = filters[4].replaceAll("%20", " ");
+      baseAll = false;
+    } else{
+      region2 = filters[5].replaceAll("%20", " ");
+      crop2 = filters[4].replaceAll("%20", " ");
       baseAll = false;
     }
   } 
   // console.log("vocation: " + vocation + ", croporreg: " + cropOrRegion);
-  return {vocation: vocation, cropOrRegion: cropOrRegion, vocation2:vocation2, cropOrRegion2:cropOrRegion2, baseAll: baseAll};
+  return {vocation: vocation, crop: crop, region: region, vocation2:vocation2, crop2:crop2, region2:region2, baseAll: baseAll};
 }
 
 export function useData(url) {
