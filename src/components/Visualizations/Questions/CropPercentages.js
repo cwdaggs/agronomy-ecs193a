c761962ba2092aa8ab1ffe454a86d49954404a25
cwdaggs <wmanu2013@gmail.com>
2022-05-30 08:37:15 -0700

    Cleaned up croppercentages

--- src/components/Visualizations/Questions/CropPercentages.js
+++ src/components/Visualizations/Questions/CropPercentages.js
@@ -1,7 +1,7 @@
 import {VictoryPie, VictoryLegend, VictoryTooltip} from 'victory';
 import {calculateCropPercentageAverage} from '../UseData.js';
 
-var colors = 
+const colors = 
 [
 "#002360",
 "#006083",
@@ -9,13 +9,18 @@ var colors =
 "#00A4A8",
 ]
 
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+const height = vw;
+const width = vw;
+const margin = { top: 0, right: 0, bottom: 0, left: 0 };
+
 function GetChart(props){
     return(
         <div class="pie-chart">
             <div class='parent flex-parent'>
                 <div class='child flex-child'>
                     <VictoryLegend
-                        colorScale={props.colors}
+                        colorScale={colors}
                         x={150}
                         y={0}  
                         gutter={20}
@@ -35,16 +40,16 @@ function GetChart(props){
                                 duration: 500,               
                             }}
                             
-                            width={props.width}
-                            height={props.height/2}
+                            width={width}
+                            height={height/2}
                             padding={{
-                                left: props.margin.left,
-                                right: props.margin.right,
-                                bottom: props.margin.bottom,
-                                top: props.margin.top
+                                left: margin.left,
+                                right: margin.right,
+                                bottom: margin.bottom,
+                                top: margin.top
                             }}
                             style={{ data: { stroke: "black", strokeWidth: 1}}}
-                            colorScale={props.colors}
+                            colorScale={colors}
                             data={props.data}
                             labels={({ datum }) => `${datum.x}: ${datum.y.toFixed() + "%"}`}
                             labelComponent={
@@ -64,19 +69,12 @@ function GetChart(props){
 }
 
 export function CropPercentages(props) {
-
     if (!props.dataset) {
         return <pre>Loading...</pre>;
     }
 
     const data = calculateCropPercentageAverage(props.dataset)
     var legend_data = []
-
-    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-    // const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
-    const height = vw;
-    const width = vw;
-    const margin = { top: 0, right: 0, bottom: 0, left: 0 };
 
     var fontSize = 20;
     for (var i = 0; i < data.length; i++) {
@@ -89,9 +87,8 @@ export function CropPercentages(props) {
                 <h2>Of the total acres, what percentage of the crops grown are in each of the following categories?</h2>
             </div>
             <div  class='visualization-window'>
-                <GetChart data={data} legend_data={legend_data} width={width} height={height} margin={margin} fontSize={fontSize} colors={colors}/>
+                <GetChart data={data} legend_data={legend_data} fontSize={fontSize}/>
             </div>
         </>
     );
 }
-
