5955712aab9538c688cfbb957b207a6895610162
unknown <darinlee888@gmail.com>
2022-06-05 15:03:15 -0700

    Fixed Acres Managed tick label overlap

--- src/components/Visualizations/Questions/AcresManaged.js
+++ src/components/Visualizations/Questions/AcresManaged.js
@@ -1,4 +1,4 @@
-import {VictoryAxis, VictoryChart, VictoryBar, VictoryTooltip} from 'victory';
+import {VictoryAxis, VictoryChart, VictoryBar, VictoryTooltip, VictoryLabel} from 'victory';
 import {filterByVocation, filterByCrop, filterByRegion} from '../UseData.js';
 import { VocationAndRegion, VocationAndRegionCompare } from "../Menus/VocationAndRegion.js";
 import {useState} from "react";
@@ -103,6 +103,13 @@ function GetChart(props){
               tickLabels: {fontSize: props.fontSize*1.25, padding: 5, fontFamily: 'Roboto'},
               axisLabel: {fontSize: props.fontSize*2, fontFamily: 'Roboto', padding: (width >= mobileWidth) ? 60: 20}
               }}
+              tickLabelComponent={       
+                <VictoryLabel    
+                    textAnchor="start"
+                    angle={25}
+                    style={{fontSize: props.fontSize}}
+                />   
+              }
           />
           <VictoryAxis dependentAxis
           label = {props.lengthString}
